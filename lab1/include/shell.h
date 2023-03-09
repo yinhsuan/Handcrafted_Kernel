59f3a8c3be6efe014ee05dab97c69ed0215b050a
yinhsuan <sandy10235168@gmail.com>
2023-03-09 14:12:19 +0800

    add cli_cmd_clear

--- lab1/include/shell.h
+++ lab1/include/shell.h
@@ -1,6 +1,9 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#define CMD_MAX_LEN 32
+
 void cli_print_banner();
+void cli_cmd_clear(char*, int);
 
 #endif
--- lab1/src/shell.c
+++ lab1/src/shell.c
@@ -6,3 +6,9 @@ void cli_print_banner() {
     uart_puts("  Welcome to NYCU-OSC 2023 Lab1 Shell  \r\n");
     uart_puts("=======================================\r\n");
 }
+
+void cli_cmd_clear(char* buffer, int length) {
+    for (int i=0; i<length; i++) {
+        buffer[i] = '\0';
+    }
+}
